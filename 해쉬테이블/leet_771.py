f913232f8c43f141a1820f75013fd985e55ffa02
Yeji <akakak82@sookmyung.ac.kr>
2020-08-03 21:09:50 +0900

    0803

--- 해쉬테이블/leet_771.py
+++ 해쉬테이블/leet_771.py
@@ -1,15 +1,10 @@
 class Solution:
-
     def numJewelsInStones(self, J: str, S: str) -> int:
-
-        
-
         J = list(J)
 
         counts = [S.count(i) for i in J]
 
         ans = sum(counts)
 
-        
-
-        return an
+    
+        return ans
